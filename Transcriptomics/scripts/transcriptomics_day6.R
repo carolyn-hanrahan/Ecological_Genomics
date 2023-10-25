7b3e5fb9f856e6460761b5d685dcbf84b980fe0d
carolyn-hanrahan <122393990+carolyn-hanrahan@users.noreply.github.com>
2023-10-25 11:36:44 -0400

    update transcriptomics_day6.R script

--- Transcriptomics/scripts/transcriptomics_day6.R
+++ Transcriptomics/scripts/transcriptomics_day6.R
@@ -13,6 +13,11 @@ if (!require("BiocManager", quietly = TRUE))
 BiocManager::install("GO.db")
 
 
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("impute")
+
 library(WGCNA);
 
 # The following setting is important, do not omit.
@@ -22,6 +27,7 @@ library(DESeq2)
 library(ggplot2)
 
 library(tidyverse)
+library(dplyr)
 
 library(remotes)
 remotes::install_github("kevinblighe/CorLevelPlot")
@@ -61,3 +67,152 @@ nrow(dds)
 
 # Run the DESeq model to test for differential gene expression
 dds <- DESeq(dds)
+
+############################################################################## Look for outliers
+
+# 2. QC - outlier detection ------------------------------------------------
+# detect outlier genes
+
+gsg <- goodSamplesGenes(t(countsTable))
+summary(gsg)
+gsg$allOK
+
+table(gsg$goodGenes)
+table(gsg$goodSamples)
+
+
+# detect outlier samples - hierarchical clustering - method 1
+htree <- hclust(dist(t(countsTable)), method = "average")
+plot(htree) 
+
+
+# pca - method 2
+
+pca <- prcomp(t(countsTable))
+pca.dat <- pca$x
+
+pca.var <- pca$sdev^2
+pca.var.percent <- round(pca.var/sum(pca.var)*100, digits = 2)
+
+pca.dat <- as.data.frame(pca.dat)
+
+ggplot(pca.dat, aes(PC1, PC2)) +
+  geom_point() +
+  geom_text(label = rownames(pca.dat)) +
+  labs(x = paste0('PC1: ', pca.var.percent[1], ' %'),
+       y = paste0('PC2: ', pca.var.percent[2], ' %'))
+
+
+############################################################################ Normalize with DESeq
+
+# 3. Normalization ----------------------------------------------------------------------
+
+colData <- row.names(sample_metadata)
+
+# making the rownames and column names identical
+all(rownames(colData) %in% colnames(countsTableRound)) # to see if all samples are present in both
+all(rownames(colData) == colnames(countsTableRound))  # to see if all samples are in the same order
+
+
+# perform variance stabilization - normalize the data and extract normalized counts
+dds_norm <- vst(dds)
+
+# dds_norm <- vst(normalized_counts)
+
+# get normalized counts
+norm.counts <- assay(dds_norm) %>% 
+  t()
+
+
+
+
+
+############################################################### Sample clustering & visualization
+
+# 4. Network Construction  ---------------------------------------------------
+# Choose a set of soft-thresholding powers
+power <- c(c(1:10), seq(from = 12, to = 50, by = 2))
+
+# Call the network topology analysis function; this step takes a couple minutes
+sft <- pickSoftThreshold(norm.counts,
+                         powerVector = power,
+                         networkType = "signed",
+                         verbose = 5)
+
+
+sft.data <- sft$fitIndices
+
+# visualization to pick power
+
+a1 <- ggplot(sft.data, aes(Power, SFT.R.sq, label = Power)) +
+  geom_point() +
+  geom_text(nudge_y = 0.1) +
+  geom_hline(yintercept = 0.8, color = 'red') +
+  labs(x = 'Power', y = 'Scale free topology model fit, signed R^2') +
+  theme_classic()
+
+
+a2 <- ggplot(sft.data, aes(Power, mean.k., label = Power)) +
+  geom_point() +
+  geom_text(nudge_y = 0.1) +
+  labs(x = 'Power', y = 'Mean Connectivity') +
+  theme_classic()
+
+
+grid.arrange(a1, a2, nrow = 2)
+# based on this plot, choose a soft power to maximize R^2 (above 0.8) and minimize connectivity
+# for these ahud data: 6-8; Higher R2 should yield more modules.
+
+
+# convert matrix to numeric
+norm.counts[] <- sapply(norm.counts, as.numeric)
+
+soft_power <- 6
+temp_cor <- cor
+cor <- WGCNA::cor # use the 'cor' function from the WGCNA package
+
+
+# this step also takes a few minutes; ideally your maxBlockSize is larger than your number of genes to run the memory-intensive network construction all at once.
+bwnet <- blockwiseModules(norm.counts,
+                          maxBlockSize = 26000,
+                          minModuleSize = 30, 
+                          reassignThreshold=0,
+                          TOMType = "signed",
+                          power = soft_power,
+                          mergeCutHeight = 0.25,
+                          numericLabels = F,
+                          randomSeed = 1234,
+                          verbose = 3)
+
+# TOMtype (Topological Overlap Matrix type) parameter - unsigned - doesn't consider positive/negative co-expression
+# signed - when you want to consider the direction of co-expression interaction, e.g., activating or inhibiting
+# WGCNA often uses a dendrogram-based approach to identify modules. The choice of the 
+# height cut in the dendrogram can determine the number of modules. Selecting a higher
+# cut height results in fewer, larger modules, while a lower cut height leads to more, 
+# smaller modules.
+
+cor <- temp_cor
+
+
+
+################################################################# Exploring Eigenvalues 
+
+# 5. Module Eigengenes ---------------------------------------------------------
+module_eigengenes <- bwnet$MEs
+
+head(module_eigengenes)
+
+
+# get number of genes for each module
+table(bwnet$colors)
+
+# Plot the dendrogram and the module colors before and after merging underneath
+plotDendroAndColors(bwnet$dendrograms[[1]], cbind(bwnet$unmergedColors, bwnet$colors),
+                    c("unmerged", "merged"),
+                    dendroLabels = FALSE,
+                    addGuide = TRUE,
+                    hang= 0.03,
+                    guideHang = 0.05)
+
+# grey module = all genes that doesn't fall into other modules were assigned to the grey module
+# with higher soft power, more genes fall into the grey module
